e806651a89512f3290e5a48f7782c02147fbcad5
Servet.Karabas <servet.karabas@gmail.com>
2018-05-23 17:15:43 +0300

    first commit

---
+++ src/main/java/com/ts/opeen/OpeenApplication.java
@@ -0,0 +1,12 @@
+package com.ts.opeen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeenApplication.class, args);
+	}
+}
